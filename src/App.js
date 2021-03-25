d7950661dca6d36c58b9a0822bba7dc42cc99bc1
Ayush Kaushik <kaushik.ayush9027@gmail.com>
2021-03-25 22:24:20 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import Navbar from './Components/Navbar'
+import Details from './Components/Details'
+import Cart from './Components/Cart'
+import Default from './Components/Default'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Navbar} />
+        <Route path='/details' component={Details} />
+        <Route path='/cart' component={Cart} />
+        <Route path='/default' component={Default} />
+      </Switch>
+    </>
+  )
 }
 
-export default App;
+export default App
